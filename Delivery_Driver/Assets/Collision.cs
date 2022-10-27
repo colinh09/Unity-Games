7116e47e1dfc661624560c786038539d894d3089
colinh09 <colin.hwang@cooper.edu>
2022-10-27 17:14:59 -0400

    created level map, still need to add collision detection to objects within game

--- Delivery_Driver/Assets/Collision.cs
+++ Delivery_Driver/Assets/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Warning: You have collided into something!");
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Passed over speed boost!");
+    }
 }
