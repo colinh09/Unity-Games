66f7c28ebc43bc914b5d5a7a679ff1f07641a054
colinh09 <colin.hwang@cooper.edu>
2022-11-01 01:27:53 -0400

    restart functionality upon win/lose event

--- Snow-Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow-Boarder/Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float delayTimer = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground"){
-            Debug.Log("You died!");
+            Invoke("ReloadScene", delayTimer);
         }
+    }
+    
+    void ReloadScene(){
+        SceneManager.LoadScene(0);        
     }
 }
--- Snow-Boarder/Assets/Scripts/FinishLine.cs
+++ Snow-Boarder/Assets/Scripts/FinishLine.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float delayTimer = 1f;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player"){
-            Debug.Log("You finished");
+            // delay the reloading of the scene after winning the game
+            Invoke("ReloadScene", delayTimer);
         }
+    }
+
+    void ReloadScene(){
+        // scene management class to use load scene method
+        // add index of scene as a parameter to loadscene method
+        // this makes so when the player crosses (triggers) the finish line, the player gets sent back to the start
+        // scenemanager is just reloading the scene upon trigger        
+        SceneManager.LoadScene(0);
     }
 }
