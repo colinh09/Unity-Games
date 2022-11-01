434ae8d3319ee1c4b96d1d5582194a4217f25613
colinh09 <colin.hwang@cooper.edu>
2022-11-01 15:52:27 -0400

    added speed up mechanic / fleshed out level

--- Snow-Boarder/Assets/Scripts/PlayerController.cs
+++ Snow-Boarder/Assets/Scripts/PlayerController.cs
@@ -6,24 +6,41 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 100f;
+    [SerializeField] float baseSpeed = 15f;
+    SurfaceEffector2D surfaceEffector2D;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    { 
+    void Update(){   
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+
+    void RespondToBoost(){
+        // if we push up, speed up, otherwise stay at normal speed
+        if(Input.GetKey(KeyCode.UpArrow)){
+            surfaceEffector2D.speed = boostSpeed;
+        } else {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer(){
         // using old input method for simplicity
         // if the player presses the left key, we want the snowboarder to spin 
         // we allow the snowboarder to rotate by adding torque to its rigid body
         if(Input.GetKey(KeyCode.LeftArrow)){
             rb2d.AddTorque(torqueAmount);
         }
-
         else if(Input.GetKey(KeyCode.RightArrow)){
             rb2d.AddTorque(-torqueAmount);
-        }        
+        }              
     }
 }
