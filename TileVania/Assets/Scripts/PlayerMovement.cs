281a0dd8dd8370e90fdb4dc66cbddbeda932d233
colinh09 <colin.hwang@cooper.edu>
2022-11-15 23:01:23 -0500

    added running/jumping functionality

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,16 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
     [SerializeField] float speed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    CapsuleCollider2D myCapsuleCollider;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -24,9 +29,23 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    private void OnJump(InputValue value){
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+            return;
+        }
+        if (value.isPressed){
+            myRigidBody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     private void Run(){
         Vector2 playerVelocity = new Vector2(moveInput.x * speed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        if (Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon){
+            myAnimator.SetBool("isRunning", true);
+        } else {
+            myAnimator.SetBool("isRunning", false);            
+        }
     }
 
     private void FlipSprite(){
