a2a5b92c37f5d7119f1e722233385d73ecdfcdb8
colinh09 <colin.hwang@cooper.edu>
2022-12-24 16:42:53 -0500

    scene persist feature

--- TileVania/Assets/Scripts/CoinPickup.cs
+++ TileVania/Assets/Scripts/CoinPickup.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int coinValue = 100;
+
+    bool collected = false;
 
     private void OnTriggerEnter2D(Collider2D other){
-        if (other.tag == "Player"){
+        if (other.tag == "Player" && !collected){
+            FindObjectOfType<GameSession>().UpdateScore(coinValue);
+            collected = true;
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
--- TileVania/Assets/Scripts/GameSession.cs
+++ TileVania/Assets/Scripts/GameSession.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+
     void Awake()
     {   
         int numGameSessions = FindObjectsOfType<GameSession>().Length;
@@ -16,6 +21,11 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    void Start(){
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
     public void ProcessPlayerDeath(){
         if (playerLives > 1){
             TakeLife();
@@ -24,14 +34,21 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    public void UpdateScore(int coinValue){
+        score += coinValue;
+        scoreText.text = score.ToString();
+    }
+
     private void TakeLife(){
         playerLives--;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
     }
 
     private void ResetGameSession(){
         SceneManager.LoadScene(0);
         Destroy(gameObject);
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
     }
 }
